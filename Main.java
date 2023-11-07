070a86977430d6b5613c71a6a0d6a868373f7aab
marcosmpuc <90069495+marcosmpuc@users.noreply.github.com>
2023-11-07 22:59:48 +0000

    Adiciona configuração.

--- Main.java
+++ Main.java
@@ -1,14 +1,29 @@
 public class Main {
 
+    public static String IPDaProximaMaquina;
+    public static String apelidoDestaMaquina;
+    public static Duration tempoParaSoltarToken;
+    public static boolean gerarToken;
+
 	public static void main(String args[]) {
         configurar();
+
+        if (gerarToken) {
+
+            gerarToken = false;
+        }
+
 		esperarPorToken();
 		String dado = esperarPorDado();
 		lidarComDado(dado);
 	}
 
     public static void configurar() {
-
+        Scanner scanner = new Scanner(new File("./config"));
+        IPDaProximaMaquina = scanner.nextLine();
+        apelidoDestaMaquina = scanner.nextLine();
+        tempoParaSoltarToken = Duration.ofSeconds(Integer.parseInt(scanner.nextLine()));
+        gerarToken = Boolean.parseBoolean(scanner.nextLine());
     }
 
     public static void esperarPorToken() {
